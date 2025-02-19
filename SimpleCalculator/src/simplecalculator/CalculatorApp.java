25f748e015dc025af303ab3552bd53de1d08a819
katyalika <lihachevak05@gmail.com>
2025-02-19 13:20:42 +0300

    gui frame
    создана панель с кнопками

--- SimpleCalculator/src/simplecalculator/CalculatorApp.java
+++ SimpleCalculator/src/simplecalculator/CalculatorApp.java
@@ -11,7 +11,12 @@ package simplecalculator;
 public class CalculatorApp {
     
     public static void main(String[] args) {
-        // TODO code application logic here
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new NewJFrame().setVisible(true);
+            }
+        });
+       
     }
     
 }
